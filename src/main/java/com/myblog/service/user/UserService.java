a36b865ca73e3a364bf3cfbe5bee8c9205399d3f
lain-jm <a331037760@gmail.com>
2018-10-12 02:13:24 +0800

    springboot 配置

--- src/main/java/com/myblog/service/user/UserService.java
+++ src/main/java/com/myblog/service/user/UserService.java
@@ -1,11 +1,13 @@
 package com.myblog.service.user;
 
 
+import com.myblog.pojo.user.User;
+
 public interface UserService {
 
-//    public User selectUser(int userId);
-//
-//    public User addUser(User user) throws Exception;
+    public User selectUser(int userId);
+
+    public User addUser(User user) throws Exception;
 
 
 }
